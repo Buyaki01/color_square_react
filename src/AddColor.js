1953b8d19710c899c1362c1535a6365c0b2bd4d8
Buyaki <rittahbuyaki@gmail.com>
2023-02-22 11:26:33 +0300

    Add AddColor component

---
+++ src/AddColor.js
@@ -0,0 +1,15 @@
+const AddColor = () => {
+  return(
+    <form>
+      <div className="text-center">
+        <input 
+          type="text" 
+          placeholder="Add color name"
+          className="border border-slate-400 rounded p-2 mt-3"
+        />
+      </div>
+    </form>
+  )
+}
+
+export default AddColor;
